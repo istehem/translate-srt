cbfc63df442f83bc8bceaad2d05f84facbb40a0c
Oskar Ingemarsson <oskari@student.chalmers.se>
2020-03-28 16:04:09 +0100

    improved time object

--- common/time.py
+++ common/time.py
@@ -4,47 +4,33 @@ import re
 
 class Time:
     def __init__(self, hours = 0, minutes = 0, seconds = 0, milliseconds = 0):
-        ms, msr = self.calc(int(milliseconds), 1000)
-        self.milliseconds = ms
-        s, sr = self.calc(int(seconds) + msr, 60)
-        self.seconds = s
-        m, mr = self.calc(int(minutes) + sr, 60)
-        self.minutes = m
-        self.hours = int(hours) + mr
-
-        self.assertvalid()
+        self.hours = 0
+        self.minutes = 0
+        self.seconds = 0
+        self.milliseconds = 0
+        self.__add_with_values(int(hours), int(minutes), int(seconds), int(milliseconds))
 
     def add(self, time : Time) -> Time:
-        mssum = self.milliseconds + time.milliseconds
-        self.milliseconds = mssum % 1000
-        msrest = 0
-        if mssum >= 1000:
-            msrest = 1
-
-        ssum = self.seconds + time.seconds + msrest
-        self.seconds = ssum % 60
-        srest = 0
-        if ssum >= 60:
-           srest = 1
-
-        msum = self.minutes + time.minutes + srest
-        self.minutes = msum % 60
-        mrest = 0
-        if msum >= 60:
-           mrest = 1
-
-        self.hours = self.hours + time.hours + mrest
-
-        self.assertvalid()
-
+        self.__add_with_values(time.hours, time.minutes, time.seconds, time.milliseconds)
         return self
-
-    def calc(self, value : int, maxval : int) -> Tuple[int, int]:
-        realval = value % maxval
-        return (realval, value // maxval)
 
     def __str__(self):
         return f'{self.hours:02}:{self.minutes:02}:{self.seconds:02},{self.milliseconds:03}'
+
+    def __add_with_values(self, hours: int, minutes : int, seconds : int, milliseconds : int) -> None:
+        ms, msr = self.__calc(self.milliseconds + milliseconds, 1000)
+        self.milliseconds = ms
+        s, sr = self.__calc(self.seconds + seconds + msr, 60)
+        self.seconds = s
+        m, mr = self.__calc(self.minutes + minutes + sr, 60)
+        self.minutes = m
+        self.hours = self.hours + hours + mr
+
+        self.assertvalid()
+
+    def __calc(self, value : int, maxval : int) -> Tuple[int, int]:
+        realval = value % maxval
+        return (realval, value // maxval)
 
     @classmethod
     def fromstr(cls, timestr : str) -> Time:
@@ -55,6 +41,3 @@ class Time:
         assert abs(self.minutes) < 60, f'minutes: maximum value 60, actual {self.minutes}'
         assert abs(self.seconds) < 60, f'seconds: maximum value 60, actual {self.seconds}'
         assert abs(self.milliseconds) < 1000, f'milliseconds: maximum value 1000, actual {self.milliseconds}'
-
-
-
